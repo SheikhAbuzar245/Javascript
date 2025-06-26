49096454e0f9681151f0727fc98ba63c62e9cf3d
SheikhAbuzar245 <abuzarsarfraz963@gmail.com>
2025-06-26 18:13:49 +0500

    Filess Added....

---
+++ Basics_02/02_arrays.js
@@ -0,0 +1,29 @@
+const num = [1 ,2 , 3] 
+const num2 = [4 , 5 ,6 ]
+
+// num.push(num2)
+
+// console.log(num);
+// console.log(num[3][1]); //  5
+
+// const number = num.concat(num2)
+// console.log(number);
+
+ 
+//  newArr = [1, 2 , 3 , [ 4, 5 ], [ 6 , 7, 8] , 9 ]
+
+//  console.log(newArr.flat(Infinity));
+ 
+// console.log(Array.isArray(num))
+// console.log(Array.from("num")) // Converts String to Array
+
+console.log(Array.of("num","Num")) 
+
+console.log(Array.of("1", "2" , "3"))   // Converts to an Array
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))   // Converts to an Array
