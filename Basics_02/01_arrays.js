54faa36cf6f7c3baa05718dc0e4d15663305d60e
SheikhAbuzar245 <abuzarsarfraz963@gmail.com>
2025-06-26 13:12:26 +0500

    Arrays Method Done....

---
+++ Basics_02/01_arrays.js
@@ -0,0 +1,27 @@
+const arr = [ 0 , 1 , 2 , 3 , 4 , 5 , 6 ]
+
+// console.log(arr)  
+
+const myArr = new Array( 1, 2 , 3 , 4 , 5 )
+
+// myArr.push(6)
+// myArr.pop()
+// myArr.unshift(8)
+// myArr.shift()
+
+// console.log(myArr.includes(7))
+// console.log(myArr.indexOf(4))
+
+// const newArr = myArr.join(arr)
+// console.log(newArr)
+
+console.log( myArr.slice(1,3) );
+console.log(myArr);
+
+console.log( myArr.splice(1,3) );
+console.log(myArr);
+
+
+
+
+
