f57c16f1504dbf3052e8e02e022e85481508097e
SheikhAbuzar245 <abuzarsarfraz963@gmail.com>
2025-07-02 14:52:42 +0500

    This Keyword & Arrow Function DOne......

--- Basics_03/02_arrow.js
+++ Basics_03/02_arrow.js
@@ -4,8 +4,46 @@ const user = {
 
     welcomeMessage: function(){
         console.log(`${this.username}, welcome to website.`);
-        
+        console.log(this);
+
     }
 
 }
 
+// user.welcomeMessage()
+// user.username = "sheikh abuzar"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function one(){
+//     let username = "abuzar"
+//     console.log(this.username);
+// }
+
+// one()
+
+
+// *********************** Arrow Function ******************** 
+
+
+// const one = ()=> {
+//     let username = "abuzar"
+//     console.log(this);
+// }
+
+// one()
+
+
+// const addTwo = (num1,num2)=>{
+//     return num1 + num2
+// }
+
+
+// const addTwo = (num1,num2) =>  num1 + num2
+
+// const addTwo = (num1,num2) =>  (num1 + num2)
+
+const addTwo = (num1,num2) =>  ( {username: "Abuzar"} )
+
+console.log(addTwo(2,3));
