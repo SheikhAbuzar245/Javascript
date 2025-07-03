a6dcab1991ba99e3f3052eff8786f3179644a31f
SheikhAbuzar245 <abuzarsarfraz963@gmail.com>
2025-07-03 21:44:17 +0500

    Control Flow All Done....

---
+++ ControlFlow_04/truthy.js
@@ -0,0 +1,68 @@
+// const userEmail = "abuzar@gmail.com"
+
+// const userEmail = ""
+
+// if (userEmail) {
+//     console.log("Got User Email..");
+// } else {
+//     console.log("Dont have user email");
+// }
+
+// falsy values
+
+// false, 0 , -0 , BigInt 0n, "", null, undefined, NaN 
+
+// truthy values
+
+// "0" , "false", " ", [], {}, function(){}, 
+
+
+// const userEmail = []
+
+// if (userEmail.length === 0) {
+//     console.log("Array is Empty");
+// }
+
+// const userEmail = {}
+// if (Object.keys(userEmail).length === 0 ) {
+//     console.log("Object is Empty");
+    
+// }
+
+
+
+
+// Nullish Coalescing Operator (??) : null undefined
+
+let val1
+
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+// val1 = null ?? 10 ?? 20
+
+console.log(val1);
+
+
+//  Ternary Operator
+
+
+// condition ? true : false
+
+const price = 120
+
+price <= 80 ?  console.log("Less than 80") : console.log("Greater than 80");
+
+
+
+
+
+// **********************************************************
+
+
+//  false == 0 // true
+
+// false == '' // true
+
+//  0 == '' // true
+
