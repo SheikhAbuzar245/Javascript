80eeae15bf3219c1495ded16bdb4a5e50db69699
SheikhAbuzar245 <abuzarsarfraz963@gmail.com>
2025-07-04 08:36:31 +0500

    While & Do While Loop

---
+++ Iterations_05/two.js
@@ -0,0 +1,23 @@
+
+
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let myArr = [2,3,4,5,6]
+// let index = 0;
+
+// while ( index < myArr.length) {
+//     console.log(`Value is ${myArr[index]}`);
+//     index ++
+    
+// }
+
+let score = 11
+
+do{
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10)
+
